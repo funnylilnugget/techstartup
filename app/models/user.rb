ff700891701f532c32eb2a91ec3ac99791e09338
noah <you@example.com>
2019-01-14 16:16:18 -0500

    minor association fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
 has_secure_password
 validates :first_name, :last_name, :username, :email, presence: true
 validates :premium, default: false
-has_many :posts
-belongs_to :location
+has_many :posts, dependent: :destroy
+belongs_to :location, optional: true
 has_one_attached :picture
 
 def full_name
