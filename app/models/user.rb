da06093eacbf2e9647f5a4967e3530ea44195212
funnylilnugget <kenny@funnylilnugget.com>
2019-01-14 16:10:12 -0500

    fixed user rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ has_secure_password
 validates :first_name, :last_name, :username, :email, presence: true
 validates :premium, default: false
 has_many :posts
-belongs_to :location
+belongs_to :location, optional: true
 has_one_attached :picture
 
 def full_name
