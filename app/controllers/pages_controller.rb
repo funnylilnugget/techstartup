3ddcd3af1620357800399490155e6910840fd0a3
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-14 19:06:31 -0500

    fixed sessions && logout button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    if !logged_in?
+      redirect_to login_path
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,18 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
   def destroy
-    user = current_user
-    log_out user
-    redirect_to '/'
+  
+    session[:user_id] = nil
+
+
+    redirect_to root_path
   end
 
 end
