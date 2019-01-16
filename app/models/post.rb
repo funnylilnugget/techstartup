8b074bfb0215bff8c4fa964c88c394ecb717df2f
George Bayley <mzpmgdkqsrnb@opayq.com>
2019-01-15 22:41:22 -0500

    octothorpe for tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-validates :name, :description, presence: true
-validates :premium, default: false
-belongs_to :user, optional: true
-belongs_to :category, optional: true
-has_one_attached :picture
+  belongs_to :user, optional: true
+  belongs_to :category, optional: true
+  has_one_attached :picture
+  validates :name, :description, :tags, presence: true
+  validates :tags, format: {with: /\A#[a-z]/}
+  validates :premium, default: false
 end
