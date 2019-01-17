e58e1a068f57837683105ec5e21b307f60f8b42f
funnylilnugget <kenny@funnylilnugget.com>
2019-01-17 16:07:04 -0500

    added delete user account funtionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,13 @@ class UsersController < ApplicationController
     end
   end
 
+def destroy
+  @user =  User.find(params[:id])
+@user.destroy
+session["user_id"] = nil
+redirect_to root_path
+end
+
   private
 
   def user_params
