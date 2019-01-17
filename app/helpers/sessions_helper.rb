ab606fe6096284b5793fbca56347f70355c0b9e1
funnylilnugget <kenny@funnylilnugget.com>
2019-01-17 14:11:06 -0500

    removed if statement from sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,9 +7,7 @@ module SessionsHelper
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
+end
   end
 
   def logged_in?
