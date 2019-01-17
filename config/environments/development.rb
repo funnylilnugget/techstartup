56bc6ddad757580f143eb6b5360d597fabcd6573
funnylilnugget <kenny@funnylilnugget.com>
2019-01-17 14:28:52 -0500

    reconfigured action mailer and added to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,8 +76,8 @@ config.action_mailer.smtp_settings = {
 address:              'smtp.gmail.com',
 port:                 587,
 domain:               'example.com',
-user_name:            'communitychestnyc',
-password:             'techstartup',
+user_name:            ENV['EMAIL_USERNAME'],
+password:             ENV['EMAIL_PASSWORD'],
 authentication:       'plain',
 enable_starttls_auto: true  }
 
