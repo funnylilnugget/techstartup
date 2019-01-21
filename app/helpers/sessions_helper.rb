8069e2eaf201c4c9c910744882bff0df3da9668e
Christopher Tobiasz <tobiasz.christopher@gmail.com>
2019-01-20 21:27:51 -0500

    minor change

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,7 @@ module SessionsHelper
   end
 
   def current_user
-    if current_user.nil?
-      redirect_to '/login'
-    end
-      if session[:user_id]
+    if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end
   end
